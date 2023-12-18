f04bd6a114db13d551e94f9f080655b6fedab5a5
Mohammed Anas FIKHI <work@gwhyyy.com>
2023-12-18 19:07:53 +0100

    [ Edit ] changes in main account page

--- components/unauthenticated/unauthenticated_body.tsx
+++ components/unauthenticated/unauthenticated_body.tsx
@@ -1,46 +1,84 @@
+import { Container } from "@mui/material";
+import { Card, CardBody, Button, Image } from "@nextui-org/react";
 import { signIn } from "next-auth/react";
-import {
-  GithubLoginButton,
-  GoogleLoginButton,
-} from "react-social-login-buttons";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 export function UnAuthenticatedBody() {
   return (
-    <div className="flex flex-col items-center justify-center py-20">
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-4xl font-bold">
-          Get your API key and start using <br />
-          <span
-            style={{
-              color: "#0077b6",
-            }}
-          >
-            LangSync
-          </span>{" "}
-          in seconds.
-        </h1>
+    <Container maxWidth="sm" className="my-10">
+      <Card>
+        <CardBody className="p-7">
+          <Title />
+          <BriefFeatures />
+          <GithubAuth />
+        </CardBody>
+      </Card>
+    </Container>
+  );
+}
 
-        <div
-          className="
-            flex flex-col
-            items-center
-            justify-center
-            flex-1
-            px-20
-            text-center
-            mt-10
-            space-y-4
-            sm:px-6 sm:flex sm:items-center sm:justify-center
-            md:flex md:items-center md:justify-center
-            lg:flex lg:items-center lg:justify-center
-            xl:flex xl:items-center xl:justify-center
-            2xl:flex 2xl:items-center 2xl:justify-center
-        "
-        >
-          <GithubLoginButton onClick={() => signIn("github")} />
-          {/* <GoogleLoginButton onClick={() => signIn("github")} /> */}
-        </div>
-      </main>
+function GithubAuth() {
+  return (
+    <div className="flex items-center py-4">
+      <Button
+        color="default"
+        onClick={() => signIn("github")}
+        endContent={<GitHubIcon />}
+      >
+        Sign in with Github
+      </Button>
     </div>
+  );
+}
+
+function Title() {
+  return (
+    <div className="flex">
+      <p
+        className="text-2xl font-bold text-left text-gradient"
+        style={{ marginTop: "1rem" }}
+      >
+        Localize your apps faster <br /> than ever.
+      </p>
+    </div>
+  );
+}
+
+function BriefFeatures() {
+  let features = [
+    "AI-powered Engine",
+    "Accurate & Fast flow",
+    "Easy to use & Integrate",
+    "Useful for any kind of software",
+  ];
+
+  return (
+    <CardBody>
+      <ul
+        className="list-disc list-inside"
+        style={{
+          listStyle: "none", // To remove bullet icon
+          paddingLeft: "0.5rem", // for decrease padding on left
+        }}
+      >
+        {features.map((feature) => {
+          return (
+            <>
+              <li key={feature}>
+                <CheckCircleOutlineIcon
+                  style={{
+                    color: "#117A2A",
+                    fontSize: "12.5px",
+                    marginBottom: "2.5px",
+                  }}
+                />{" "}
+                {feature}
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </CardBody>
   );
 }
